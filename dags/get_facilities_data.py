8b7f9ec8a6c90adb473be7e4760b796cc817ec72
jy016011 <39334789+jy016011@users.noreply.github.com>
2024-07-19 16:44:25 +0900

    [feat] add extracting facility detail data task in DAG to get facilities data - extract_facility_datail_data: Task that extracts facility detail data - and add feature that load detail data to s3 raw bucket in 'load_to_s3_raw' task

--- dags/get_facilities_data.py
+++ dags/get_facilities_data.py
@@ -1,3 +1,5 @@
+import time
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
@@ -29,7 +31,7 @@ def fetch_page(api_url, params=None, headers=None):
     return None
 
 
-def fetch_all_pages(api_url, params=None):
+def fetch_all_facilities(api_url, params=None):
     all_data = []
     page = 1
 
@@ -44,6 +46,25 @@ def fetch_all_pages(api_url, params=None):
         all_data.extend(parse_xml_facilities(page_data))
 
         page += 1
+
+    df = pd.DataFrame(all_data)
+
+    return df
+
+
+def fetch_all_facility_details(api_url, fclt_ids, params=None):
+    all_data = []
+
+    for idx, fclt_id in enumerate(fclt_ids, start=1):
+        detail_api_url = api_url + f'/{fclt_id}'
+        logging.info(f'Fetching {idx}th item {fclt_id}')
+        page_data = fetch_page(detail_api_url, params)
+
+        if not page_data or '<db>' not in page_data:
+            break
+
+        all_data.extend(parse_xml_facility_details(page_data))
+        time.sleep(0.5)
 
     df = pd.DataFrame(all_data)
 
@@ -69,9 +90,32 @@ def parse_xml_facilities(xml_data):
     return all_data
 
 
+def parse_xml_facility_details(xml_data):
+    xml_root = ET.fromstring(xml_data)
+
+    # XML 데이터를 순회하며 필요한 데이터 추출
+    all_data = []
+    for db in xml_root.findall('db'):
+        row = {
+            'fcltynm': db.find('fcltynm').text, # 시설명
+            'mt10id': db.find('mt10id').text, # 공연 시설 ID
+            'mt13cnt': db.find('mt13cnt').text, # 공연장 수
+            'fcltychartr': db.find('fcltychartr').text, # 시설특성
+            'opende': db.find('opende').text, # 개관연도
+            'seatscale': db.find('seatscale').text, #객석 수
+            'telno': db.find('telno').text,# 전화번호
+            'relateurl': db.find('relateurl').text, # 홈페이지
+            'adres': db.find('adres').text, # 주소
+            'la': db.find('la').text, # 위도
+            'lo': db.find('lo').text, # 경도
+        }
+        all_data.append(row)
+    return all_data
+
+
 def extract_facilities_data(**kwargs):
     api_url = 'http://kopis.or.kr/openApi/restful/prfplc'
-    api_key = Variable.get('kopis_api_key')
+    api_key = Variable.get('kopis_api_key_facility')
     api_key_decode = requests.utils.unquote(api_key)
     seoul_area_code = 11
     params = {
@@ -80,11 +124,27 @@ def extract_facilities_data(**kwargs):
         'signgucode': seoul_area_code,
     }
 
-    fetched_df = fetch_all_pages(api_url, params)
+    fetched_df = fetch_all_facilities(api_url, params)
     print(f'Fetched {len(fetched_df)} items')
 
     return fetched_df.to_json()
 
+
+def extract_facility_detail_data(**kwargs):
+    api_url = 'http://kopis.or.kr/openApi/restful/prfplc'
+    api_key = Variable.get('kopis_api_key_facility')
+    api_key_decode = requests.utils.unquote(api_key)
+    params = {
+        'service': api_key_decode
+    }
+
+    df_json = kwargs['ti'].xcom_pull(task_ids='extract_facilities_data')
+    df = pd.read_json(df_json)
+
+    fetched_df = fetch_all_facility_details(api_url, df['mt10id'], params)
+    print(f'Fetched {len(fetched_df)} items')
+
+    return fetched_df.to_json()
 
 
 def load_to_s3_raw(**kwargs):
@@ -92,31 +152,44 @@ def load_to_s3_raw(**kwargs):
     bucket = 'hellokorea-raw-layer'
 
     # # Pull merged DataFrame from XCom
-    facilities_data_json = ti.xcom_pull(task_ids='extract_facilities_data')
-    df = pd.read_json(facilities_data_json)
+    facility_list_json = ti.xcom_pull(task_ids='extract_facilities_data')
+    df_facility_list = pd.read_json(facility_list_json)
+    facility_detail_json = ti.xcom_pull(task_ids='extract_facility_detail_data')
+    df_facility_detail = pd.read_json(facility_detail_json)
 
     # Convert DataFrame to CSV string
-    csv_buffer = io.StringIO()
-    df.to_csv(csv_buffer, index=False)
-    csv_string = csv_buffer.getvalue()
+    facility_list_csv_string = convert_to_csv_string(df_facility_list)
+    facility_detail_csv_string = convert_to_csv_string(df_facility_detail)
 
     # Define S3 path
     execution_date = kwargs['execution_date']
     kst_date = convert_to_kst(execution_date)
-    s3_key = f'source/kopis/facilities/{kst_date.year}/{kst_date.month}/{kst_date.day}/facilities_{kst_date.strftime("%Y%m%dT%H%M%S")}.csv'
+    facility_list_s3_key = f'source/kopis/facilities/{kst_date.year}/{kst_date.month}/{kst_date.day}/facilities_{kst_date.strftime("%Y%m%dT%H%M%S")}.csv'
+    facility_detail_s3_key = f'source/kopis/facility_detail/{kst_date.year}/{kst_date.month}/{kst_date.day}/facility_detail{kst_date.strftime("%Y%m%dT%H%M%S")}.csv'
 
     # Upload to S3
     s3 = S3Hook(aws_conn_id='s3_conn')
-    s3.load_string(
-        string_data=csv_string,
-        key=s3_key,
-        bucket_name=bucket
-    )
+    upload_to_s3(s3, facility_list_s3_key, facility_list_csv_string, bucket)
+    upload_to_s3(s3, facility_detail_s3_key, facility_detail_csv_string, bucket)
 
 
 def convert_to_kst(execution_date):
     kst = pytz.timezone('Asia/Seoul')
     return execution_date.astimezone(kst)
+
+
+def convert_to_csv_string(df):
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+    return csv_buffer.getvalue()
+
+
+def upload_to_s3(s3, s3_key,csv_string, bucket):
+    s3.load_string(
+        string_data=csv_string,
+        key=s3_key,
+        bucket_name=bucket
+    )
 
 
 # Define default_args
@@ -142,10 +215,16 @@ extract_facilities_data = PythonOperator(
     dag=dag,
 )
 
+extract_facility_detail_data = PythonOperator(
+    task_id='extract_facility_detail_data',
+    python_callable=extract_facility_detail_data,
+    dag=dag,
+)
+
 load_to_s3_raw = PythonOperator(
     task_id='load_to_s3',
     python_callable=load_to_s3_raw,
     dag=dag,
 )
 
-extract_facilities_data >> load_to_s3_raw
+extract_facilities_data >> extract_facility_detail_data >>load_to_s3_raw
